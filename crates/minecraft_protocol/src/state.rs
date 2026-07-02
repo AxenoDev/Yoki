c0b158db0650021e17d5e9e6a453f798accc9c48
Timeo Bavart <tbavart@gmail.com>
2026-07-02 10:11:13 +0200

    refactor: use strum macros for Display/AsRefStr implementations

--- crates/minecraft_protocol/src/state.rs
+++ crates/minecraft_protocol/src/state.rs
@@ -1,40 +1,26 @@
-use std::fmt::{Display, Formatter, Result};
+use strum_macros::{Display, AsRefStr};
 
-#[derive(Copy, Debug, PartialEq, Clone, Eq, Hash)]
-pub enum Direction {
+#[derive(Copy, Debug, PartialEq, Clone, Eq, Hash, Display, AsRefStr)]
+pub enum DirectionBound {
+    #[strum(serialize = "clientbound")]
     Clientbound,
+    #[strum(serialize = "serverbound")]
     Serverbound,
 }
 
-#[derive(Copy, Debug, PartialEq, Clone, Default, Eq, Hash)]
+#[derive(Copy, Debug, PartialEq, Clone, Default, Eq, Hash, Display, AsRefStr)]
 pub enum State {
     #[default]
+    #[strum(serialize = "handshake")]
     Handshaking,
+    #[strum(serialize = "status")]
     Status,
+    #[strum(serialize = "login")]
     Login,
+    #[strum(serialize = "configuration")]
     Configuration,
+    #[strum(serialize = "play")]
     Play,
+    #[strum(serialize = "transfer")]
     Transfer,
-}
-
-impl Display for State {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        match self {
-            State::Handshaking => f.write_str("handshake"),
-            State::Status => f.write_str("status"),
-            State::Login => f.write_str("login"),
-            State::Configuration => f.write_str("configuration"),
-            State::Play => f.write_str("play"),
-            State::Transfer => f.write_str("transfer"),
-        }
-    }
-}
-
-impl Display for Direction {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        match self {
-            Direction::Clientbound => f.write_str("clientbound"),
-            Direction::Serverbound => f.write_str("serverbound"),
-        }
-    }
 }
